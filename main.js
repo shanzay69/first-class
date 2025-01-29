bc8b1b38b73f47db2aa8d836f2392fb358d0b04f
shanzay <meriqainat69@gmail.com>
2025-01-29 17:03:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//Template literals
+//let firstname="shanza khan"
+//let fatherName="sardar"
+//console.log(`hello my name is ${firstname}  and my fathername is ${fatherName}`);
+//unary operators {slide no 117}
+//(++), (--) {prefix and postfix operators}
+//let index=10
+//++index  // 10+1
+//++index // 11+1
+//++index // 12+1
+//++index // 13+1
+//++index // 14+1
+//console.log(index);
+var index1 = 10;
+++index1; // 10+1 =11
+--index1; // 11-1 =10
+++index1; // 10+1 =11
+--index1; // 11-1 =10
+--index1; // 10-1 =9
+console.log(index1);
